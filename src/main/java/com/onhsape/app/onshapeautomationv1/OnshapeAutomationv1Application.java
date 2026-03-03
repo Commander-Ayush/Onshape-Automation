98199730205181ec0c68beb4d3282e3e99ce4ab6
Commander-Ayush <ayushkrjha221004@gmail.com>
2026-03-03 12:55:00 +0530

    Initialized

--- src/main/java/com/onhsape/app/onshapeautomationv1/OnshapeAutomationv1Application.java
+++ src/main/java/com/onhsape/app/onshapeautomationv1/OnshapeAutomationv1Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OnshapeAutomationv1Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OnshapeAutomationv1Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OnshapeAutomationv1Application.class, args);
+    }
 
 }
