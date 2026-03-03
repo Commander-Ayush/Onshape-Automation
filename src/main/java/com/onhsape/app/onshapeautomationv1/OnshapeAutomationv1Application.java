72357e0d74abdcf25a6cd7392dc519630028860d
Commander-Ayush <ayushkrjha221004@gmail.com>
2026-03-03 12:54:35 +0530

    first commit

---
+++ src/main/java/com/onhsape/app/onshapeautomationv1/OnshapeAutomationv1Application.java
@@ -0,0 +1,13 @@
+package com.onhsape.app.onshapeautomationv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnshapeAutomationv1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnshapeAutomationv1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/onhsape/app/onshapeautomationv1/OnshapeAutomationv1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onhsape.app.onshapeautomationv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnshapeAutomationv1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
